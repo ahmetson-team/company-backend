e98ff286fcc88cb99238d941de42a66b667a709d
enthuzzed <kyle.hammmmm@gmail.com>
2023-11-27 19:34:25 +0700

    package json script added

--- cashbackAPI.js
+++ cashbackAPI.js
@@ -21,7 +21,7 @@ app.get('/shop/price/:product_id', (req, res) => {
 });
 
 // Start the server on port 3001 (or any desired port)
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
