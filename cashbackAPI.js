e6204151b21dcdda3470bdae90bdf25881701312
enthuzzed <kyle.hammmmm@gmail.com>
2023-11-29 15:28:32 +0700

    Add a POST method that receives data from the smartcontract

--- cashbackAPI.js
+++ cashbackAPI.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+
+// Parse incoming JSON requests
+app.use(bodyParser.json());
+
 
 // Mock loyalty points data (replace with actual logic to fetch points for product IDs)
 const loyaltyPoints = {
@@ -19,6 +25,18 @@ app.get('/shop/price/:product_id', (req, res) => {
         res.status(404).json({ error: 'Product ID not found' });
     }
 });
+
+// Define the endpoint to receive user data
+app.post('/receive-user-data', (req, res) => {
+    // Retrieve data from the request body
+    const { user_address, receipt_id, credential_id, user_data } = req.body;
+
+    // Perform operations with the received data (logic to process user data)
+    // For now, just send back a success response with loyalty points
+    const loyaltyPoints = 100;
+    res.json({ loyalty_points: loyaltyPoints });
+});
+
 
 // Start the server on port 3001 (or any desired port)
 const PORT = process.env.PORT || 3001;
